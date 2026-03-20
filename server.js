7a3e145a4ce012a6fbf866414c4b52c23add6af3
karthikeyans2006 <karthik3259m@gmail.com>
2026-03-20 19:41:59 +0530

    Fix server listen - bind to 0.0.0.0

--- server.js
+++ server.js
@@ -626,4 +626,7 @@ app.get('/api/moviesda/download', async (req, res) => {
   }
 });
 
-export default app;
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on http://0.0.0.0:${PORT}`);
+});
